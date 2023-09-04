d55aa81aca755c330c554d0666580316cd358cf5
Jelson Stoelben Rodrigues <jelsonrodrigues25@gmail.com>
2023-09-04 17:05:31 -0300

    terrain lighting started

--- src/shaders/terrainVS.glsl
+++ src/shaders/terrainVS.glsl
@@ -7,10 +7,12 @@ layout( location=0 ) in vec4 position;
 
 out vec2 f_text_coord;
 out vec3 f_normal;
+out vec3 f_position;
 
 uniform mat4x4 u_projection;
 uniform mat4x4 u_view;
 uniform mat4x4 u_model;
+uniform mat4x4 u_inverse_transpose_model;
 
 uniform highp sampler2D u_heightmap;
 
@@ -22,15 +24,15 @@ If the surface was defined by a function then it would be possible to use calcul
 get the exact normal.
 */
 vec3 calculate_normal(vec4 point0) {
-  float dist = (1.0/(8192.0 * 2.0));
-  vec3 neigbor1 = vec3(position.x + dist, position.y, position.z);
-  vec3 neigbor2 = vec3(position.x, position.y, position.z + dist);
+  float dist = (1.0/(8192.0 * 8.0));
+  vec2 neigbor1 = vec2(position.x + dist, position.z);
+  vec2 neigbor2 = vec2(position.x, position.z + dist);
 
-  vec4 height1 = texture(u_heightmap, neigbor1.xz) * vec4(1.0, 1.0/256.0, 0.0, 0.0);
-  vec4 point1 = vec4(neigbor1.x, height1.r + height1.g, neigbor1.z, 1.0);
+  vec4 height1 = texture(u_heightmap, neigbor1.xy) * vec4(1.0, 1.0/256.0, 0.0, 0.0);
+  vec4 point1 = vec4(neigbor1.x, height1.r + height1.g, neigbor1.y, 1.0);
 
-  vec4 height2 = texture(u_heightmap, neigbor2.xz) * vec4(1.0, 1.0/256.0, 0.0, 0.0);
-  vec4 point2 = vec4(neigbor2.x, height2.r + height2.g, neigbor2.z, 1.0);
+  vec4 height2 = texture(u_heightmap, neigbor2.xy) * vec4(1.0, 1.0/256.0, 0.0, 0.0);
+  vec4 point2 = vec4(neigbor2.x, height2.r + height2.g, neigbor2.y, 1.0);
 
   vec4 tangent1 = point1 - point0;
   vec4 tangent2 = point2 - point0;
@@ -45,7 +47,8 @@ void main() {
   vec4 height = texture(u_heightmap, f_text_coord) * vec4(1.0, 1.0/256.0, 0.0, 0.0);
   vec4 this_point = vec4(position.x, (height.r + height.g), position.z, 1.0);
   gl_Position = u_projection * u_view * u_model * this_point;
-
-  f_normal = mat3(transpose(inverse(u_model))) * calculate_normal(this_point);
+  
+  f_position = (u_model * this_point).xyz;
+  f_normal = mat3(u_inverse_transpose_model) * calculate_normal(this_point);
 }
 
--- src/ts/Light/PointLightDot.ts
+++ src/ts/Light/PointLightDot.ts
@@ -7,8 +7,9 @@ import vertexSource from "../../shaders/vertexShader.glsl";
 import fragmentSource from "../../shaders/fragmentShader.glsl";
 import { Camera } from "../Camera/Camera";
 import { Light } from "../Light/Light";
+import { PointLight } from "./PointLight";
 
-export class Origin implements DrawableObject {
+export class PointLightDot extends PointLight implements DrawableObject{
   public model : glm.mat4;
 
   private static initialized : boolean = false;
@@ -22,32 +23,37 @@ export class Origin implements DrawableObject {
   private static u_color : WebGLUniformLocation;
   private static vertices : number = 0;
 
-  constructor (gl : WebGL2RenderingContext) {
+  constructor (gl : WebGL2RenderingContext, position : glm.vec3, color : glm.vec3, radius : number) {
+    super(position, color, radius);
     this.model = glm.mat4.create();
+    this.model[12] = position[0];
+    this.model[13] = position[1];
+    this.model[14] = position[2];
     
-    if (!Origin.initialized) {
+    if (!PointLightDot.initialized) {
       this.setup(gl);
     }
 
-    Origin.initialized = true;
+    PointLightDot.initialized = true;
   }
 
   draw(gl : WebGL2RenderingContext, camera : Camera, projection : glm.mat4, lights : Light[]) : void {
     const view = camera.getViewMatrix();
-    gl.useProgram(Origin.program as WebGLProgram);
+    gl.useProgram(PointLightDot.program as WebGLProgram);
 
-    gl.bindVertexArray(Origin.vao);
+    gl.bindVertexArray(PointLightDot.vao);
     
-    gl.uniformMatrix4fv(Origin.u_model, false, this.model);
-    gl.uniformMatrix4fv(Origin.u_view, false, view);
-    gl.uniformMatrix4fv(Origin.u_projection, false, projection);
+    gl.uniformMatrix4fv(PointLightDot.u_model, false, this.model);
+    gl.uniformMatrix4fv(PointLightDot.u_view, false, view);
+    gl.uniformMatrix4fv(PointLightDot.u_projection, false, projection);
 
-    gl.uniform4f(Origin.u_color, 1.0, 1.0, 0.5, 1.0);
+    const color = super.getColor();
+    gl.uniform4f(PointLightDot.u_color, color[0], color[1], color[2], 1.0);
 
     gl.drawArrays(
       WebGL2RenderingContext.POINTS,
       0,
-      Origin.vertices
+      PointLightDot.vertices
     );
 
     // Unbind VAO to other gl calls do not modify it
@@ -56,52 +62,48 @@ export class Origin implements DrawableObject {
   
   setup(gl: WebGL2RenderingContext): void {
     // Create the program
-    Origin.program = WebGLUtils.createProgram(
+    PointLightDot.program = WebGLUtils.createProgram(
       gl,
       WebGLUtils.createShader(gl, WebGL2RenderingContext.VERTEX_SHADER, vertexSource) as WebGLShader,
       WebGLUtils.createShader(gl, WebGL2RenderingContext.FRAGMENT_SHADER, fragmentSource) as WebGLShader
     ) as WebGLProgram;
-    gl.useProgram(Origin.program);
+    gl.useProgram(PointLightDot.program);
     
     // Look up uniform and attributes positions
-    Origin.u_model = gl.getUniformLocation(Origin.program, "model") as WebGLUniformLocation;
-    Origin.u_view = gl.getUniformLocation(Origin.program, "view") as WebGLUniformLocation;
-    Origin.u_projection = gl.getUniformLocation(Origin.program, "projection") as WebGLUniformLocation;
-    Origin.u_color = gl.getUniformLocation(Origin.program, "color") as WebGLUniformLocation;
+    PointLightDot.u_model = gl.getUniformLocation(PointLightDot.program, "model") as WebGLUniformLocation;
+    PointLightDot.u_view = gl.getUniformLocation(PointLightDot.program, "view") as WebGLUniformLocation;
+    PointLightDot.u_projection = gl.getUniformLocation(PointLightDot.program, "projection") as WebGLUniformLocation;
+    PointLightDot.u_color = gl.getUniformLocation(PointLightDot.program, "color") as WebGLUniformLocation;
     
-    Origin.a_position = gl.getAttribLocation(Origin.program, "position");
+    PointLightDot.a_position = gl.getAttribLocation(PointLightDot.program, "position");
 
     // Create the vertices buffer
-    Origin.buffer_vertices = gl.createBuffer() as WebGLBuffer;
+    PointLightDot.buffer_vertices = gl.createBuffer() as WebGLBuffer;
 
     // Create the Vertex Array Object
-    Origin.vao = gl.createVertexArray() as WebGLVertexArrayObject;    
-    gl.bindVertexArray(Origin.vao);
+    PointLightDot.vao = gl.createVertexArray() as WebGLVertexArrayObject;    
+    gl.bindVertexArray(PointLightDot.vao);
     
     // Tell VAO what buffer to bind
-    gl.bindBuffer(WebGL2RenderingContext.ARRAY_BUFFER, Origin.buffer_vertices);
+    gl.bindBuffer(WebGL2RenderingContext.ARRAY_BUFFER, PointLightDot.buffer_vertices);
 
     // Tell it how to read Data
     gl.vertexAttribPointer(
-      Origin.a_position,
+      PointLightDot.a_position,
       3, 
       WebGL2RenderingContext.FLOAT, 
       false, 
       3 * Float32Array.BYTES_PER_ELEMENT, 
       0 * Float32Array.BYTES_PER_ELEMENT
     );
-    gl.enableVertexAttribArray(Origin.a_position);
+    gl.enableVertexAttribArray(PointLightDot.a_position);
 
     // Unbind VAO buffer so other objects cannot modify it
     gl.bindVertexArray(null);
 
-    gl.bindBuffer(WebGL2RenderingContext.ARRAY_BUFFER, Origin.buffer_vertices);
+    gl.bindBuffer(WebGL2RenderingContext.ARRAY_BUFFER, PointLightDot.buffer_vertices);
     const data = new Float32Array([
-      -1.0, 0.0, -1.0,
-      1.0, 0.0, -1.0,
-      -1.0, 0.0, 1.0,
-      1.0, 0.0, 1.0,
-      0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0
     ]);
     gl.bufferData(
       WebGL2RenderingContext.ARRAY_BUFFER,
@@ -109,6 +111,6 @@ export class Origin implements DrawableObject {
       WebGL2RenderingContext.STATIC_DRAW
     );
 
-    Origin.vertices = data.length / 3;
+    PointLightDot.vertices = data.length / 3;
   }
 }
